97720dd18ef77f93c3f5ea45e6abeb7df411ade0
DorisPineda <113275278+DorisPineda@users.noreply.github.com>
2022-09-15 17:11:53 -0600

    Add files via upload

---
+++ PeriodoClase6Ejercicio2.java
@@ -0,0 +1,20 @@
+package periodoclase6ejercicio2;
+public class PeriodoClase6Ejercicio2 {
+    public static void main(String[] args) {
+        // Programa Java para demostrar el uso de un
+        // string para controlar una declaración switch
+        String str = "dos";
+        switch(str)
+        {
+            case "uno":
+                System.out.println("uno");
+            case "dos":
+                System.out.println("dos");
+            case "tres":
+                System.out.println("tres");
+            default:
+                System.out.println("no coincide");
+        }
+}
+    
+}
