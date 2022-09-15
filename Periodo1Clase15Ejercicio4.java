45df5912f4ea1de0d0106abf1a8cb07f11b72003
DorisPineda <113275278+DorisPineda@users.noreply.github.com>
2022-09-15 15:22:25 -0600

    Add files via upload

---
+++ Periodo1Clase15Ejercicio4.java
@@ -0,0 +1,13 @@
+package periodo1clase15ejercicio4;
+import java.util.Scanner;
+public class Periodo1Clase15Ejercicio4 {
+    public static void main(String[] args) {
+        Scanner Entrada = new Scanner(System.in);
+        System.out.println("Ingrese la base: ");
+        double base = Entrada.nextDouble();
+        System.out.println("Ingrese el exponente: ");
+        double exponenente = Entrada.nextDouble();
+        double resultado = Math.pow(base, exponente);
+        System.out.println("El resultado es: " + resultado);
+    }
+}
